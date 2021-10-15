39dea9f11fc55485729e9435ed198b18b4db7ea6
Veprintsev Boris <45390259+BorisVeprintzev@users.noreply.github.com>
2021-10-15 16:38:28 +0300

    auth and global chat

--- hw5/hw5main.go
+++ hw5/hw5main.go
@@ -1,32 +1,36 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"github.com/dgrijalva/jwt-go"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
-const SecretKey = "ABOBA"
-
-type Token string
+const cookieAuth = "auth"
+const userID = "ID"
 
 var CountUser uint64 = 0
 
-type IDMessageType map[int]string
-type LoginTokenType map[string]Token
-type TokenLoginType map[Token]string
-type IDPersonalMessageType map[int]map[int]string
+type Message struct {
+	Author  string
+	Message string
+}
 
-var UserSlice = make([]User, 10)
-var IdMessage = make(IDMessageType)
-var LoginToken = make(LoginTokenType)
-var IdPersonalMessage = make(IDPersonalMessageType)
-var TokenLogin = make(TokenLoginType)
+type NewMessage struct {
+	Message string `json:"message"`
+}
+
+var UserSlice = make([]User, 0)
+var GlobalChat = make([]Message, 0)
 
 //func Auth(handler http.Handler) http.Handler {
 //	fn := func(w http.ResponseWriter, r *http.Request) {
@@ -53,7 +57,18 @@ var TokenLogin = make(TokenLoginType)
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello"))
+	_, _ = w.Write([]byte("Hello"))
+}
+
+func Hello2(w http.ResponseWriter, r *http.Request) {
+	id, ok := r.Context().Value(userID).(string)
+	if !ok {
+		log.Error("Error read context")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Hello2, " + id))
 }
 
 func PostPersonalChatHandler(w http.ResponseWriter, r *http.Request) {
@@ -65,84 +80,161 @@ func GetPersonalChatHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostGroupChatHandler(w http.ResponseWriter, r *http.Request) {
-
+	name, ok := r.Context().Value(userID).(string)
+	if !ok {
+		log.Error("Error read context, PostGroupChatHandler")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Info("Start add new Message. Read body.")
+	text, err := io.ReadAll(r.Body)
+	log.Info(fmt.Sprintf("body request: %s", string(text)))
+	defer r.Body.Close()
+	var nMessage NewMessage
+	var message Message
+	if err != nil {
+		log.Error("Can't read body request. PostGroupChatHandler.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = json.Unmarshal(text, &nMessage)
+	log.Info(fmt.Sprintf("Read new Message %s", nMessage.Message))
+	message.Author = name
+	message.Message = nMessage.Message
+	GlobalChat = append(GlobalChat, message)
+	log.Info(fmt.Sprintf("New Message %+v, was add to global chat.", message))
 }
 
 func GetGroupChatHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func (u *User) GenerateToken(w http.ResponseWriter) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userid": u.ID,
-		"exp":    time.Now().Add(time.Hour * 106).Unix(),
-	})
-	tokenString, err := token.SignedString([]byte(SecretKey))
-	if err != nil {
-		w.Write([]byte("Token give error"))
+	_, ok := r.Context().Value(userID).(string)
+	if !ok {
+		log.Error("Error read context. GetGroupChatHandler")
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	u.Token = Token(tokenString)
+	log.Info("Search global chat messages")
+	for _, message := range GlobalChat {
+		_, err := w.Write([]byte(fmt.Sprintf("User: %s, Message: %s\n", message.Author, message.Message)))
+		if err != nil {
+			log.Error("Unexpected error, read global Message")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+	log.Info("End send global chat messages")
 }
 
-func SendTokenToUser(w http.ResponseWriter, r *http.Request) {
+type LoginStruct struct {
+	Login string `json:"login"`
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
 	text, err := io.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
+		log.Error("Can't read body request")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	var login LoginStruct
 	var user User
-	err = json.Unmarshal(text, &user)
-	if err != nil || user.Username == "" {
+	err = json.Unmarshal(text, &login)
+	if err != nil || login.Login == "" {
+		log.Error("Can't read login")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	user.Username = login.Login
 	user.ID = CountUser
 	CountUser++
-	user.GenerateToken(w)
-	UserSlice = append(UserSlice, user)
-	answer, err := json.Marshal(user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Error marshal"))
+	c := &http.Cookie{
+		Name:  cookieAuth,
+		Value: user.Username,
+		Path:  "/",
 	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(answer)
+	user.CookieValue = c.Value
+	UserSlice = append(UserSlice, user)
+	log.Info(fmt.Sprintf("Add new User: %s, summury count users: %d", user.Username, CountUser))
+	log.Info(fmt.Sprintf("Add new cookie to user %s", user.Username))
+	log.Info(fmt.Sprintf("User %s get new cookie: %s", user.Username, user.CookieValue))
+	http.SetCookie(w, c)
+}
+
+func Auth(handler http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		c, err := r.Cookie(cookieAuth)
+		switch err {
+		case nil:
+		case http.ErrNoCookie:
+			log.Error("Empty cookie")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		default:
+			log.Error("Unexpected error")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		var u User
+		u.ID = 100
+		log.Info(fmt.Sprintf("Get cookie:%s", c.Value))
+		for _, user := range UserSlice {
+			log.Info(fmt.Sprintf("Check user %s, cookie:%s", user.Username, user.CookieValue))
+			if user.CookieValue == c.Value {
+				log.Info(fmt.Sprintf("User %s found", user.Username))
+				u = user
+				break
+			}
+		}
+		if u.ID == 100 {
+			log.Error("User doesn't found")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		userCtx := context.WithValue(r.Context(), userID, c.Value)
+
+		handler.ServeHTTP(w, r.WithContext(userCtx))
+	}
+	return http.HandlerFunc(fn)
 }
 
 type User struct {
-	ID       uint64 `json:"id"`
-	Username string `json:"username"`
-	Token    Token  `json:"token"`
+	ID          uint64 `json:"id"`
+	Username    string `json:"username"`
+	CookieValue string `json:"cookie"`
 }
 
 func main() {
+	authRoute := chi.NewRouter()
 	server := http.Server{
 		Addr:        ":4999",
-		Handler:     nil,
+		Handler:     authRoute,
 		ReadTimeout: time.Second,
 	}
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	logger := log.New()
 
-	authRoute := chi.NewRouter()
 	authRoute.Use(middleware.RequestID)
 	authRoute.Use(middleware.Logger)
-	authRoute.Post("/getToken", SendTokenToUser)
+	authRoute.Post("/login", Login)
 	authRoute.Get("/", Hello)
 
 	logger.Info("start authRoute")
-	//mainRoute := chi.NewRouter()
-	//mainRoute.Use(middleware.RequestID)
-	//mainRoute.Use(middleware.Logger)
-	//// mainRoute.Use(Auth)
-	//mainRoute.Get("/messages", GetGroupChatHandler)
-	//mainRoute.Post("/messages", PostGroupChatHandler)
-	//mainRoute.Get("/messages/me", GetPersonalChatHandler)
-	//mainRoute.Post("/messages/{userName}", PostPersonalChatHandler)
+	mainRoute := chi.NewRouter()
+	mainRoute.Use(middleware.RequestID)
+	mainRoute.Use(middleware.Logger)
+	mainRoute.Use(Auth)
+	mainRoute.Get("/messages", GetGroupChatHandler)
+	mainRoute.Post("/messages", PostGroupChatHandler)
+	mainRoute.Get("/messages/me", GetPersonalChatHandler)
+	mainRoute.Post("/messages/{userName}", PostPersonalChatHandler)
+	mainRoute.Get("/hello", Hello2)
 
+	authRoute.Mount("/auth", mainRoute)
 	log.Info("Wait requests")
 	log.Fatal(server.ListenAndServe())
+	<-c
+	time.Sleep(3 * time.Second)
 }
